f0df969e99ca8e7f4901762d505b5371d11458d3
jiangyunhua <421747209@qq.com>
2022-04-30 13:46:35 +0800

    first commit

---
+++ src/main/java/com/yunhua/controller/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.yunhua.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yunhua
+ * @version 1.0
+ * @description 功能描述
+ * @create 2022/4/30 13:36
+ */
+@RestController
+public class HelloWorld {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world hh";
+    }
+
+}
